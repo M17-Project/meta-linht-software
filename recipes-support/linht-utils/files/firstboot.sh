f4810ad32f765a690dfa9df26e0b42b4f9ab4ab8
OE3ANC <dev@oe3anc.at>
2025-11-21 17:29:58 +0100

    Added first boot service for flowgraph preperation and alsa default values

--- recipes-support/linht-utils/files/firstboot.sh
+++ recipes-support/linht-utils/files/firstboot.sh
@@ -2,10 +2,26 @@
 set -e
 echo "Running first-boot script..."
 
-touch /usr/share/linht/setup_done
+systemctl stop gui-test
+
 cd /usr/share/linht/grc
 grcc /usr/share/linht/grc/som_m17_ptt.grc
 
-systemctl restart gui-test
+systemctl start gui-test
 
-echo "First-boot script completed."
+amixer -q -c wm8960audio sset 'Speaker' 127,127
+amixer -q -c wm8960audio sset 'Speaker AC' 5
+amixer -q -c wm8960audio sset 'Speaker DC' 4
+
+amixer -q -c wm8960audio sset 'Right Boost Mixer RINPUT1' on
+amixer -q -c wm8960audio sset 'Right Input Boost Mixer RINPUT1' 0
+amixer -q -c wm8960audio sset 'Right Input Mixer Boost' on
+amixer -q -c wm8960audio sset 'ADC Data Output Select' 'Left Data = Right ADC; Right Data = Right ADC'
+
+alsactl store
+
+echo "First-boot script completed, disabling service."
+
+# TODO -> remove if it works with ExecStartPost now
+# systemctl disable first-boot.service
+
